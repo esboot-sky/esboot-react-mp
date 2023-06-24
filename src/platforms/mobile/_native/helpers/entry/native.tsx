36869cb4288a2681acbd1d62a427ce6197c514dd
Roc <moonlitusun@gmail.com>
2023-06-24 18:50:23 +0800

    del lint-staged依赖

--- src/platforms/mobile/_native/helpers/entry/native.tsx
+++ src/platforms/mobile/_native/helpers/entry/native.tsx
@@ -25,6 +25,6 @@ export default function wrapNative(App): React.ReactNode {
     );
   };
 
-  InternalApp.displayName = `wrapNative1(${getDisplayName(App)})`;
+  InternalApp.displayName = `wrapNative12(${getDisplayName(App)})`;
   return <InternalApp />;
 }
