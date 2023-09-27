9a713e1afd6eba1ded0809f1e423a77559eb1ec3
donghs <donghs@dztec.net>
2023-09-27 15:13:20 +0800

    feat: first commit

---
+++ src/platforms/mobile/_browser/modules/index/app.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
+import './app.scss';
+
+const App = () => (
+  <div styleName="main">
+    Mobile Browser Platform
+    <Outlet />
+  </div>
+);
+
+export default App;
