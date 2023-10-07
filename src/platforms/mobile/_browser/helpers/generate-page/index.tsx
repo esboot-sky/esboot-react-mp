b8db0da9da43f577ab2cd8cd6d3a6dc9aa423f2c
donghs <donghs@dztec.net>
2023-10-07 20:43:33 +0800

    feat: 支持mobile generate page

---
+++ src/platforms/mobile/_browser/helpers/generate-page/index.tsx
@@ -0,0 +1,24 @@
+import { mounteReact } from '@/helpers/react';
+
+import wrapI18n, { I18nOption } from '@mobile/hoc/i18n';
+import { wrapRedux } from '@/hoc/redux';
+import { wrapReactQuery } from '@/hoc/query-client';
+
+import '@/styles/index.scss';
+import '@mobile/styles/index.scss';
+
+interface GeneratePageOptions {
+  store: any;
+  i18n?: I18nOption;
+}
+
+export default function generatePage(App: React.ReactNode, options: GeneratePageOptions): void {
+  const { i18n, store } = options;
+  let wrapApp: React.ReactNode = App;
+
+  if (i18n) wrapApp = wrapI18n(wrapApp, i18n);
+
+  wrapApp = wrapReactQuery(wrapApp);
+  wrapApp = wrapRedux(wrapApp, store);
+  mounteReact(wrapApp as React.ReactElement);
+}
