95d446f0af434e1c58dc195a3a7a4450b901fbce
donghs <donghs@dztec.net>
2023-10-07 15:48:11 +0800

    fix: 更新esboot配置

--- .esbootrc.ts
+++ .esbootrc.ts
@@ -1,5 +1,56 @@
-import { defineConfig } from '@dz-web/esboot';
+import { defineConfig, CodeSplittingType } from '@dz-web/esboot';
 
-export default defineConfig({
+export default defineConfig((runtimeCfg) => ({
+  // port: 8081,
+  analyze: false,
+  /**
+   * 调试库兼容性问题时，可以关闭mfsu
+   */
   mfsu: true,
-});
+  pxtorem: {
+    enable: true,
+    rootValue: runtimeCfg.isMobile ? 200 : 13,
+  },
+  extraBabelIncludes: [
+    /filter-obj/i,
+    /immer/i,
+    /zustand/i,
+    /query-string/i,
+    /react-intl/i,
+    /common/i,
+    /d3-/i,
+    /@tanstack/i,
+    /@react-spring/i,
+    /@floating-ui/i,
+  ],
+  codeSplitting: {
+    jsStrategy: CodeSplittingType.granularChunks,
+    jsStrategyOptions: {
+      frameworkBundles: [
+        '@dz-web/bridge',
+        '@dz-web/request',
+        'dayjs',
+        '@tanstack/react-query',
+        'redux',
+        'redux-thunk',
+        'react-redux',
+        '@reduxjs/toolkit',
+        'zustand',
+        'immer',
+        'lodash',
+        'nanoid',
+        '@dz-web/axios',
+        '@dz-web/axios-middlewares',
+        'axios',
+        'react-intl',
+        '@loadable/component',
+        'classnames',
+        'perfect-scrollbar',
+      ]
+    },
+  },
+}));
+
+export const afterHooks = (cfg) => {
+  console.log(cfg.entry, '<-- cfg');
+}
