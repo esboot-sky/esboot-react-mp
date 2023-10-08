d702e28ceb725d38dc52e0c754ce86e9423c4382
donghs <donghs@dztec.net>
2023-10-08 13:52:30 +0800

    feat: 添加@dz-web/axios, 模拟跳转登录失效

--- src/platforms/mobile/customize.ts
+++ src/platforms/mobile/customize.ts
@@ -48,4 +48,5 @@ listenReactQueryError((friendlyMessage, error, meta) => {
   console.error('还未添加全局错误提示', friendlyMessage, error, meta);
 });
 
-// TODO: 监听请求失败错误
+// TODO: 监听请求失败错误, 通常不统一报错，而是在各个页面自己处理，否则有错误定制需求的页面会重复报错
+// 或者使用react query管理请求，重试请求失败后，会统一处理报错，并且可以通过meta抑制错误提示
