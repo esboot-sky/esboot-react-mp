cfe39596f26805bdff1500f328e57e1c58e4b3f5
donghs <donghs@dztec.net>
2023-11-01 11:21:22 +0800

    fix: 修复vconsole初始化位置不对

--- src/platforms/mobile/helpers/v-console.ts
+++ src/platforms/mobile/helpers/v-console.ts
@@ -1,8 +1,8 @@
 import VConsole from 'vconsole';
+import { enableVConsole } from '@/constants/config';
 
-const enable = true;
-
-if (enable && process.env.NODE_ENV === 'development') {
+if (enableVConsole && process.env.NODE_ENV === 'development') {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const vConsole = new VConsole();
+  vConsole.setSwitchPosition(0, 80);
 }
