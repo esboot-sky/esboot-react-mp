ab4f11a0b27af68c64efce8e1b704f81c9ad5300
donghs <donghs@dztec.net>
2024-07-23 15:49:20 +0800

    feat: 支持关闭某个页面的React StrictMode, 方便迁移古董代码功能

--- src/helpers/react.tsx
+++ src/helpers/react.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-export function mounteReact(innerApp: React.ReactElement) {
-  createRoot(document.getElementById('root') as Element).render(<StrictMode>{innerApp}</StrictMode>);
+export function mounteReact(innerApp: React.ReactElement, disableStrictMode = false) {
+  const RootApp = disableStrictMode ? innerApp : <StrictMode>{innerApp}</StrictMode>;
+
+  createRoot(document.getElementById('root') as Element).render(RootApp);
 }
